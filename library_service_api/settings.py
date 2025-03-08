1136bfaac8f354785b3cc326bda839884db5b8b7
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-08 14:47:32 +0200

    feat: register User model in settings.py

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
