f6af87e2209964e840dee1b0e6c5c59d053eca61
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-08 18:38:36 +0200

    fix: fix wrong path to books urls

--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/users/", include("users.urls", namespace="users")),
-    path("api/v1/books/", include("books.urls", namespace="books")),
+    path("api/v1/", include("books.urls", namespace="books")),
 ]
