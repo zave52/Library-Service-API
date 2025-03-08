ff17c7d7c1ecfb32fe6a8538972b04c18702be2e
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-08 18:16:31 +0200

    feat: create Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,35 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import UniqueConstraint
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD", _("Hard")
+        SOFT = "SOFT", _("Soft")
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=150)
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverChoices,
+        default=CoverChoices.SOFT
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=(MinValueValidator(0),)
+    )
+
+    class Meta:
+        constraints = (
+            UniqueConstraint(
+                fields=("title", "author"),
+                name="unique_title_author"
+            ),
+        )
+
+    def __str__(self) -> str:
+        return self.title
