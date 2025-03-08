9a1342b29cc1b13a5f4b643e327f7017dad8eb14
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-09 00:39:57 +0200

    feat: create BorrowingViewSet with list and retrieve action

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.db.models import QuerySet
+from rest_framework import viewsets, mixins, serializers
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingListSerializer,
+    BorrowingRetrieveSerializer
+)
+
+
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Borrowing.objects.all()
+
+    def get_serializer_class(self) -> type(serializers.ModelSerializer):
+        if self.action == "list":
+            return BorrowingListSerializer
+        return BorrowingRetrieveSerializer
+
+    def get_queryset(self) -> QuerySet:
+        queryset = self.queryset
+        if self.action == "retrieve":
+            queryset = queryset.select_related("book")
+
+        return queryset
