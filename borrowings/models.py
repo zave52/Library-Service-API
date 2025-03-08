6db62a26062f6a5a8ccca9a0f2421ae7ccd78552
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-09 00:36:12 +0200

    fix: add missing import in borrowings/modelds.py

--- borrowings/models.py
+++ borrowings/models.py
@@ -4,13 +4,15 @@ from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.db import models
 
+from books.models import Book
+
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(blank=True, null=True)
     book = models.ForeignKey(
-        "Book",
+        Book,
         on_delete=models.CASCADE,
         related_name="borrowings"
     )
