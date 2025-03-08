e2c239dd314a7524c559574dbdbf4ab681ca16a3
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-08 16:52:02 +0200

    refactor: change register path in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 from users.views import CreateUserView, ManageUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="register_user"),
+    path("", CreateUserView.as_view(), name="register_user"),
     path("me/", ManageUserView.as_view(), name="manage_user"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
