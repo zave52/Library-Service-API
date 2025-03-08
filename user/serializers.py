0e059ed9fed1eb1b4145d4b8305e3c81cd978e96
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-08 15:02:14 +0200

    feat: create UserSerializer

---
+++ user/serializers.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+        "id", "email", "first_name", "last_name", "password", "is_staff")
+        read_only_fields = ("id", "is_staff")
+        extra_kwargs = {
+            "password": {
+                "write_only_field": True,
+                "min_length": 5,
+                "style": {"input_password": "password"},
+                "label": _("Password"),
+            }
+        }
+
+    def create(self, validated_data: dict) -> get_user_model():
+        """Create user with encrypted password."""
+        return get_user_model().objects.create(**validated_data)
+
+    def update(
+        self, instance: get_user_model(), validated_data: dict
+    ) -> get_user_model():
+        """Update user model with encrypted password."""
+
+        password = validated_data.get("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
