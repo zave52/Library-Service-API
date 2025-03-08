777e39928ecb6cab6dc90b5333ac0e674d99ea15
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-09 00:43:23 +0200

    feat: register Borrowing model in admin page

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from borrowings.models import Borrowing
+
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    pass
