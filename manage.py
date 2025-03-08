9b2676fb135094ecba3de922ca6acdc091b01dc5
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-08 14:40:41 +0200

    feat: configure load environment variables

--- manage.py
+++ manage.py
@@ -4,10 +4,17 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_service_api.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "library_service_api.settings"
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
